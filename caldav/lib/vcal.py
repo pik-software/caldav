bf1b6cebc1d90d30beb7cdd293d7dd5c9d28fb61
Tobias Brox <tobias@redpill-linpro.com>
2023-01-26 19:40:40 +0100

    syntax error

--- caldav/lib/vcal.py
+++ caldav/lib/vcal.py
@@ -134,7 +134,7 @@ def create_ical(ical_fragment=None, objtype=None, language="en_DK", **props):
         ## (otherwise we cannot easily add a task to a davical calendar and
         ## then find it again - ref https://gitlab.com/davical-project/davical/-/issues/281
         if not props.get('STATUS') and not '\nSTATUS:' in ical_fragment and objtype == 'VTODO':
-        props['STATUS'] = 'NEEDS-ACTION'
+            props['STATUS'] = 'NEEDS-ACTION'
 
     else:
         if not ical_fragment.startswith("BEGIN:VCALENDAR"):
