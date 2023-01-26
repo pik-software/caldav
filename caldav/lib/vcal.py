cb9598ac2bfb7c4a271b5998fb51803dd622c4e3
Tobias Brox <tobias@redpill-linpro.com>
2023-01-26 19:40:48 +0100

    style

--- caldav/lib/vcal.py
+++ caldav/lib/vcal.py
@@ -133,8 +133,12 @@ def create_ical(ical_fragment=None, objtype=None, language="en_DK", **props):
         ## STATUS should default to NEEDS-ACTION for tasks, if it's not set
         ## (otherwise we cannot easily add a task to a davical calendar and
         ## then find it again - ref https://gitlab.com/davical-project/davical/-/issues/281
-        if not props.get('STATUS') and not '\nSTATUS:' in ical_fragment and objtype == 'VTODO':
-            props['STATUS'] = 'NEEDS-ACTION'
+        if (
+            not props.get("STATUS")
+            and not "\nSTATUS:" in ical_fragment
+            and objtype == "VTODO"
+        ):
+            props["STATUS"] = "NEEDS-ACTION"
 
     else:
         if not ical_fragment.startswith("BEGIN:VCALENDAR"):
